a6f849b9426701fca1ddbbeee67213a0f8001d79
raufsemi <raufisemi@gmail.com>
2026-06-15 10:45:27 +0800

    Initial Release

--- app/data/bookingData.ts
+++ app/data/bookingData.ts
@@ -15,6 +15,7 @@ export interface TimeSlot {
 export const ROOMS: Room[] = [
   { id: 'A', name: 'Bilik  Interaksi', label: 'Bilik  Interaksi', capacity: '20 Pax',teacherOnly: true },
   { id: 'B', name: 'Bilik  Serbaguna', label: 'Bilik  Serbaguna', capacity: '20 Pax' ,teacherOnly: true},
+  { id: 'H', name: 'Bilik  Pemikiran Kreatif', label: 'Bilik  Pemikiran Kreatif', capacity: '20 Pax' ,teacherOnly: true},
   { id: 'C', name: 'Bilik  Perbincangan 1', label: 'Bilik  Perbincangan 1', capacity: '4 Pax' },
   { id: 'D', name: 'Bilik  Perbincangan 2', label: 'Bilik  Perbincangan 2', capacity: '4 Pax' },
   { id: 'E', name: 'Bilik  Perbincangan 3', label: 'Bilik  Perbincangan 3', capacity: '8 Pax' },
