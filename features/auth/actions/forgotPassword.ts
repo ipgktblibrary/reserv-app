f37daec4eee7f81486ccc49c0cfa479c676874ba
raufsemi <raufisemi@gmail.com>
2026-08-05 09:20:27 +0800

    fix(forgot-password) : change redirectTo to correct link

--- features/auth/actions/forgotPassword.ts
+++ features/auth/actions/forgotPassword.ts
@@ -15,7 +15,7 @@ export async function forgotPassword(
   const normalizedEmail = email.trim().toLowerCase();
 
   const { error } = await supabase.auth.resetPasswordForEmail(normalizedEmail, {
-    redirectTo: "http://localhost:3001/reset-password",
+    redirectTo: "https://ipgktb-library.vercel.app/reset-password",
   });
 
   if (error) {
