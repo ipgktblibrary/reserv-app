1b4a219518fade8c3656e2889a969e7b9bcf5ecf
raufsemi <raufisemi@gmail.com>
2026-09-04 15:35:47 +0800

    fix: update resetPassword function to return user data and remove signOut call

--- features/auth/actions/resetPassword.ts
+++ features/auth/actions/resetPassword.ts
@@ -5,12 +5,13 @@ import { createClient } from "@/lib/supabase/server";
 export async function resetPassword(password: string) {
   const supabase = await createClient();
 
-  const { error } = await supabase.auth.updateUser({
+  const { data, error } = await supabase.auth.updateUser({
     password,
   });
 
   if (error) {
     throw new Error(error.message);
   }
-  await supabase.auth.signOut();
+
+  return data;
 }
